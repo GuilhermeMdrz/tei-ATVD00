73d95e7d13961ea709a204d117350c2fbfa2da2f
Thais <thais.g@escolar.ifrn.edu.br>
2026-05-22 17:12:39 -0300

    resolução da questão taxa metabólica

---
+++ taxaMA.py
@@ -0,0 +1,7 @@
+W = int(input("Peso (kg): "))
+H = int(input("Altura (Metros): "))
+A = int(input("Idade: "))
+
+conta = 10.W + 6,25.H - 5.A + 5
+
+taxaMA = print("A sua taxa metabólica é: {conta:.1f}")
